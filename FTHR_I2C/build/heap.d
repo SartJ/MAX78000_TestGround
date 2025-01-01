ae1043df8b0d4fca30681a057dd38b580d0e8e72
SartJ <sartajjc@gmail.com>
2025-01-01 16:12:04 -0600

    FTHR_I2C tried out

---
+++ FTHR_I2C/build/heap.d
@@ -0,0 +1,34 @@
+/home/ectf/Desktop/MAX78000_TestGround/FTHR_I2C/build/heap.o: \
+ /home/ectf/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Source/heap.c \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/stdint.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_default_types.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/features.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/_newlib_version.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_intsup.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_stdint.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/errno.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/errno.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/reent.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/_ansi.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/newlib.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/config.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/ieeefp.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_types.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_types.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/lock.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/unistd.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/unistd.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/cdefs.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/types.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/endian.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/_endian.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/select.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_sigset.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_timeval.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/timespec.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_timespec.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/sys/sched.h \
+ /home/ectf/MaximSDK/Tools/GNUTools/10.3/arm-none-eabi/include/machine/types.h
---
+++ FTHR_I2C/build/startup_max78000.d
@@ -0,0 +1,2 @@
+/home/ectf/Desktop/MAX78000_TestGround/FTHR_I2C/build/startup_max78000.o: \
+ /home/ectf/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
